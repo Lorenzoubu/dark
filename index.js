8d559bfa46206c46b3d78de166d111ede8921739
Lorenzoubu <34841078+Lorenzoubu@users.noreply.github.com>
2018-01-05 22:51:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,88 +1,53 @@
-const Discord = require ("Discord.js");
-var bot = new Discord.Client();
-var msg = new Discord.Message()
+const Discord = require ("discord.js")
+var bot = new Discord.Client()
+var prefixbot = ("!")
+var GAME = 0
+var hasard = 0
 var prefix = ("/")
-var msgnb = 0
-//login
-bot.login();
-//Ready
-bot.on("ready", function()
-  {
-    console.log("----------------");
-    console.log("le bot est en 0.1.0 FIXE \n---------------- \nChargement . . .")
-    console.log("00000000  00    00")
-    console.log("00    00  00   0")
-    console.log("00    00  00 00 ")
-    console.log("00    00  000")
-    console.log("00    00  00 00")
-    console.log("00    00  00   0")
-    console.log("00000000  00    00")
-    console.log("Le bot est connecté \n----------------")
-  })
-//setgame
-bot.on("ready", function(){
-  bot.user.setPresence({ game: { name: "/help", type: 0}})
+
+bot.on('ready', () => {
+    console.log("Bot Ready !")
 })
-//new member
-bot.on("guildMemberAdd", function(member){
-  member.guild.channels.find("name", "bienvenue").sendMessage(member.toString() + " Bienvenue à toi")
-  console.log("Nouveau membre")
+
+bot.login(process.env.TOKEN)
+
+bot.on('message', message => {
+    if(message.content == "Ping"){
+        message.reply("Pong")
+        console.log('ping pong')
+    }
+
+    if(message.content == prefix + "help"){
+        message.channel.sendMessage("__**Voici les commandes du bot:**__\n__/help__ : afficher les *commandes du bot*.\n__!roulette__ : jouer à la *roulette* (les règles sont éxpliquées une fois la commande efféctuée).")
+        console.log("Help demandé")
+    }
+
+    
+    if(message.content == prefixbot + "roulette" & message.channel.name == "general"){
+        message.reply("Tu ne peut pas utiliser cette commande ici,\nessaye le salon espace-bot :wink: !")
+    }
+    if((message.content == prefixbot + "roulette" & message.channel.name == "espace-bot") & (GAME == 1)){
+        message.reply("Tu ne peut pas effecteur cette commande tant qu'une autre partie de *roulette* est en cours !")
+    }
+    if((message.content == prefixbot + "roulette" & message.channel.name == "espace-bot") & (GAME == 0)){
+        message.channel.sendMessage("C'est partit pour la roulette :raised_hands: !")
+        message.channel.sendMessage("Tu dois essayer de trouver un nombre compris entre 0 et 1000 :neutral_face: \nLe premier à trouver le nombre gagne la partie :upside_down: !\nPour gagner il te suffit de dire un nombre et je te dirai si c'est + ou - :wink: ")
+        GAME = 1
+        hasard = Math.floor ( Math.random() * 1001 )
+
+    }
+    if(GAME === 1 & message.channel.name == "espace-bot"){
+        if(message.content == hasard){
+            message.reply("Bravo ! Tu remportes la partie !")
+            GAME = 0
+        }
+        if(message.content < hasard){
+            message.reply("c'est +")
+        }
+        if(message.content > hasard){
+            message.reply("c'est -")
+        }
+    }
+    
 })
-//Lien
-bot.on("message", function(message) {
-  var discriminateur = message.author.discriminator
-  var membername = message.author.username
-  if(message.content.startsWith('https://')){
-    message.delete()
-      .then(msg => console.log("Deleted message from " + membername + "#" + discriminateur))
-      .catch(console.error);
-    message.channel.sendMessage("Message supprimer Raison: Lien")
-      }
-  if(message.content.startsWith('http')){
-    message.delete()
-      .then(msg => console.log("Deleted message from " + membername + "#" + discriminateur))
-      .catch(console.error);
-    message.channel.sendMessage("Message supprimer Raison: Lien")
-  }
-  if(message.content === prefix + "help"){
-    var help_Embed = new Discord.RichEmbed()
-      .setColor("#E5012E")
-      .addField("Les commande du Bot !", "   /help: Affiche le list des commande\n/msgnb: Affiche les Stats des message du serveur")
-      .addField("Interation:", "   /jeux: Pour jouer au jeux")
-    message.channel.sendEmbed(help_Embed)
-  }
-})
-bot.on("message", function(message){
-  msgnb = parseInt(msgnb) + 1
-  if(message.content === prefix + "msgnb"){
-    var msgnb_Embed = new Discord.RichEmbed()
-      .setColor("#E5012E")
-      .addField("Les Message du Bot:  ", "   Nombre Message Du serveur: " + msgnb)
-    message.channel.sendEmbed(msgnb_Embed)
-  }
-})
-//jeux
-bot.on("message", function(message){
-  if (Google.match(message)){
-    Google.action(message)
-  }
-})
-bot.on("message", function(message){
-  if (message.content == prefix + "jeux"){
-    mode = 1
-    var jeux_Embed = new Discord.RichEmbed()
-      .setColor("#E5012E")
-      .addField("Voici le jeux il faut arriver a l'argent pour gagner 10 palacoins :money_with_wings: :money_with_wings: ", "   !! Jouer en Discord Plein écrant !!")
-      .addField("Comment jouer ???", "   Avec les touches W-A-S-D comme dans les jeux FPS")
-      .addField(":o::o::o::o::o::o::o::o::o::o::o::o::o::o::o::o::o::o::o::o:", ":soccer: :white_small_square::white_small_square::white_small_square::o::o::o::o::o::o::o::o::o::o::o::o::o::o::o::o:")
-      .addField(":o::o::o::white_small_square::white_small_square::o::o::o::o::o::o::o::o::o::o::o::o::o::o::o:", ":o::o::o::o::white_small_square::white_small_square::white_small_square::white_small_square::white_small_square::white_small_square::white_small_square::o::o::o::o::o::o::o::o::o:")
-      .addField(":o::o::o::o::o::o::o::o::o::o::white_small_square::o::o::o::o::o::o::o::o::o:", ":o::o::o::o::o::o::o::o::o::o::white_small_square::o::o::o::o::o::o::o::o::o:")
-      .addField(":o::o::o::o::o::o::o::o::o::white_small_square::white_small_square::o::o::o::o::o::o::o::o::o:", ":o::o::o::o::o::o::o::o::o::white_small_square::o::o::o::o::o::o::o::o::o::o:")
-      .addField(":o::o::o::o::o::o::o::o::o::white_small_square::o::o::o::o::o::o::o::o::o::o:", ":o::o::o::o::o::o::o::o::o::white_small_square::white_small_square::o::o::o::o::o::o::o::o::o:")
-      .addField(":o::o::o::o::o::o::o::o::o::o::white_small_square::white_small_square::white_small_square::white_small_square::white_small_square::white_small_square::white_small_square::white_small_square::white_small_square::euro:", ":o::o::o::o::o::o::o::o::o::o::o::o::o::o::o::o::o::o::o::o:")
-    message.channel.sendEmbed(jeux_Embed)
-  }
-  //if(mode == 1){
-      //if (message.content == w)
-  //}
-})
+
